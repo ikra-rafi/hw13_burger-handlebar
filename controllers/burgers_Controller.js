bd039c89f9d2c4f597cc1808c3835e9c1142a2eb
Ikra Rafi <irafi@nlihc.org>
2021-02-18 03:11:16 -0500

    modified handlebars, input isn't registering on page

--- controllers/burgers_Controller.js
+++ controllers/burgers_Controller.js
@@ -19,6 +19,7 @@ router.get('/', (req, res) => {
 });
 
 router.post('/api/burgers', (req, res) => {
+  //
   burgers.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
     // Send back the ID of the new quote
     res.json({ id: result.insertId });
@@ -45,17 +46,17 @@ router.put('/api/burgers/:id', (req, res) => {
   );
 });
 
-// router.delete('/api/burgers/:id', (req, res) => {
-//   const condition = `id = ${req.params.id}`;
+router.delete('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
 
-//   burgers.delete(condition, (result) => {
-//     if (result.affectedRows === 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     }
-//     res.status(200).end();
-//   });
-// });
+  burgers.delete(condition, (result) => {
+    if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,13 +1,13 @@
 USE burgers_db;
 
-INSERT INTO burgers (burger_name, devoured)
-VALUES ("hamburger",false), ("cheeseburger",false), ("fish burger", false);
+-- INSERT INTO burgers (burger_name, devoured)
+-- VALUES ("hamburger",false), ("cheeseburger",false), ("fish burger", false);
 
 
 
--- INSERT INTO cats (name) VALUES ('Molly');
--- INSERT INTO cats (name) VALUES ('Charlie');
--- INSERT INTO cats (name, sleepy) VALUES ('Poppy', true);
--- INSERT INTO cats (name, sleepy) VALUES ('Oscar', true);
--- INSERT INTO cats (name, sleepy) VALUES ('Smudge', true);
--- INSERT INTO cats (name) VALUES ('Daisy');
+-- INSERT INTO burgers (burger_name) VALUES ('Molly');
+-- INSERT INTO burgers (burger_name) VALUES ('Charlie');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwestern Chicken Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Everything Halal Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Medium Rare Steak Burger', false);
+-- INSERT INTO burgers (burger_name) VALUES ('Daisy');
